513f35c2abadf9126837ec7c9859b2b2b4b79411
santiyunikas <santiyunikas@gmail.com>
2020-11-10 10:46:17 +0700

    update pembahasan 80%

--- app/src/main/java/com/santiyunikas/mathgeo/view/content/materi/MateriImageDialogFragment.kt
+++ app/src/main/java/com/santiyunikas/mathgeo/view/content/materi/MateriImageDialogFragment.kt
@@ -1,27 +1,29 @@
 package com.santiyunikas.mathgeo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.DialogFragment
 import com.bumptech.glide.Glide
 
-class BlankFragment : Fragment() {
+class ImageDialogFragment : DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_image_dialog, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val url = arguments?.getString("IMAGE_KUBUS_URL")
         val imgKubus: ImageView = view.findViewById(R.id.img_kubus)
+        Log.d("Masuk", url)
         Glide.with(view)
             .load(url)
             .into(imgKubus)
